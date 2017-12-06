213c3785bf912606ac2a1bd4f587754410964073
qiweichao <qiweichao@guoshujinfu.com>
2017-12-06 10:30:57 +0800

    update .gitignore

---
+++ src/main/java/com/spmjava/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.spmjava.controller;
+
+/**
+ * @author qi-wei-chao
+ * @description:
+ * @date 2017-12-06 9:54
+ * @since 1.0.0
+ **/
+
+public class IndexController {
+    public static void main(String args[]){
+        System.out.println("11");
+    }
+}
